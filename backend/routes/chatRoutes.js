3e0efcd6a7ab2419c5e40310428459e7da895e63
6string-guy <sanskarjaiswal010@gmail.com>
2024-06-04 18:32:24 +0530

    all chat routes fixed

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express'
-import { accessChat } from '../controllers/chatControllers';
+import {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  removeFromGroup,
+  addToGroup,
+} from "../controllers/chatControllers";
 import { protect } from '../middleware/authMiddleware';
 const router = express.Router();
 
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
-router.route('/group').post(protect, createdGroupChat);
+router.route('/group').post(protect, createGroupChat);
 //because database is being updated we have put request
 router.route('/rename').put(protect, renameGroup);
 router.route('/groupremove').put(protect, removeFromGroup);
