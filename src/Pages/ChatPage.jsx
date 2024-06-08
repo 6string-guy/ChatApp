f1988f5ecc9ca34299b70fbec456de556321952c
6string-guy <sanskarjaiswal010@gmail.com>
2024-06-09 00:10:42 +0530

    fixed

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer.jsx";
 import { Box } from "@chakra-ui/react";
 import MyChats from "../components/MyChats.jsx";
-import ChatBox from "../ChatBox.jsx";
+import ChatBox from "../components/ChatBox.jsx";
 
 const ChatPage = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState();
 
   return (
     <div className="w-full">
@@ -18,8 +19,8 @@ const ChatPage = () => {
         h="91.5vh"
         p="10px"
       >
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
       </Box>
     </div>
   );
