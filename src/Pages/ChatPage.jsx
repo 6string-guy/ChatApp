fbbb803b2a7cdf13755ecdd1aae4edc0cae685da
Sanskar Jaiswal <149059535+6string-guy@users.noreply.github.com>
2024-09-26 11:42:43 +0530

    Update ChatPage.jsx

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer.jsx";
+import { Box } from "@chakra-ui/react";
 import MyChats from "../components/MyChats.jsx";
 import ChatBox from "../components/ChatBox.jsx";
 
@@ -9,20 +10,18 @@ const ChatPage = () => {
   const [fetchAgain, setFetchAgain] = useState();
 
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="w-full">
       {user && <SideDrawer />}
-      <div className="flex justify-between w-full h-[91.5vh] p-2 bg-gray-100">
-        {user && (
-          <div className="flex w-1/3 bg-white shadow-lg rounded-lg p-4">
-            <MyChats fetchAgain={fetchAgain} />
-          </div>
-        )}
-        {user && (
-          <div className="flex w-2/3 bg-white shadow-lg rounded-lg p-4 ml-4">
-            <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-          </div>
-        )}
-      </div>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+      </Box>
     </div>
   );
 };
